e56e851f7f9f8fc6bbc0267764bd7a9e983e1d3e
Oliver-915 <oliver.albert915@gmail.com>
2026-03-12 11:51:52 -0500

    Update Chapter 9 team project.py

--- Chapter 9 team project.py
+++ Chapter 9 team project.py
@@ -3,32 +3,38 @@
 def main():
     #Main accepts no arguments
     #main should call all other functions to build the entire program
+    pass
 
 def get_shift():
     #get_shift accepts no arguments
     #get_shift should prompt the user for the shift value and return the value as a string
     #Validate for integers 1-25, inclusive.
+    pass
     
 def choose_option():
     #choose_option accepts no arguments
     #choose_option should prompt the user to choose to encode or decode
     #It should return True if the user chooses encode and False if the user chooses decode
     #Validate to only accept the appropriate prompts for encode and decode.
+    pass
 
 def get_message():
     #get_message accepts no argument
     #get_message should prompt the user to enter a message to encode or decode
     #It should return that message
+    pass
 
 def create_key(shift):
     #create_key accepts (shift) as an argument
     #create_key should accept the shift value from get_shift
     #It should create the caesar cipher according to the shift value and store the key in a dictionary and return the dictionary as the key
+    pass
 
 def encode(message, key):
     #encode accepts (message and key) as arguments
     #encode should accept message as a string and key as a dictionary.
     #It should encode the message using the key and return the encoded message as a string
+    pass
     
 def decode(message, key):
     #decode accepts (message and key) as arguments
